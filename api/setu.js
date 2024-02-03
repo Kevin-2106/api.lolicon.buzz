a785ca881acbe73500b2b89edfd086f7f6263681
Kev06 <68720986+Kevin-2106@users.noreply.github.com>
2024-02-03 01:31:37 +0000

    remake

--- api/setu.js
+++ api/setu.js
@@ -1,9 +1,14 @@
-const fetch = require("node-fetch");
+// 在 /api/redirect.js 文件中
+import { get } from 'axios';
 
-module.exports = async (req, res) => {
-  const response = await fetch('https://api.ln.app/setu/v2');
-  const json = await response.json();
-  const originalUrl = json.data.urls.original;
-
-  res.redirect(307, originalUrl);
+export default async (req, res) => {
+  try {
+    const response = await get('https://api.lolicon.app/setu/v2');
+    const redirectUrl = response.data.data.urls.original;
+    
+    res.setHeader('Location', redirectUrl);
+    res.status(307).end();
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred while trying to fetch the URL' });
+  }
 };
