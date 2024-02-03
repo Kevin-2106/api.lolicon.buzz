e08c1814bbade3f97f565099ef1fb47a81631ce7
Kev06 <68720986+Kevin-2106@users.noreply.github.com>
2024-02-03 01:48:36 +0000

    another attempt#2

--- api/setu.js
+++ api/setu.js
@@ -2,12 +2,22 @@ import axios from 'axios';
 
 export default async (req, res) => {
   try {
-    const response = await axios.get('https://api.lolicon.app/setu/v2');
+    const response = await axios.get('https://api.ln.app/setu/v2', { params: req.query });
     const redirectUrl = response.data.data.urls.original;
     
     res.setHeader('Location', redirectUrl);
     res.status(307).end();
   } catch (error) {
-    res.status(500).send({ error: 'An error occurred while trying to fetch the URL' });
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      res.status(error.response.status).send(error.response.data);
+    } else if (error.request) {
+      // The request was made but no response was received
+      res.status(500).send({ error: 'No response received from the server' });
+    } else {
+      // Something happened in setting up the request that triggered an Error
+      res.status(500).send({ error: 'An error occurred while trying to fetch the URL' });
+    }
   }
 };
