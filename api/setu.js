bc704f2f3ee7a643230dcbd89588d133a39f39b9
Kev06 <68720986+Kevin-2106@users.noreply.github.com>
2024-02-03 01:49:57 +0000

    lolicon

--- api/setu.js
+++ api/setu.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default async (req, res) => {
   try {
-    const response = await axios.get('https://api.ln.app/setu/v2', { params: req.query });
+    const response = await axios.get('https://api.lolicon.app/setu/v2', { params: req.query });
     const redirectUrl = response.data.data.urls.original;
     
     res.setHeader('Location', redirectUrl);
